1600e86258d67848ff3d9f449c94417b1a3fbb8f
serbantimofte <serban-timofte@outlook.com>
2024-12-10 19:26:43 +0200

    SpringBoot Start

---
+++ spring_dta/src/test/java/eu/ase/ro/spring_mvc/SpringDtaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.ase.ro.spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
